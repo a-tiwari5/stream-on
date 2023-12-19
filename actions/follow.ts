b8c3caca695aa13f712ce4d60366ffe02877fbb5
Adarsh <adarsh,tiwari@tensorgo.com>
2023-12-20 03:44:06 +0530

    Feature:Follow service added

---
+++ actions/follow.ts
@@ -0,0 +1,32 @@
+"use server";
+
+import { followUser, unfollowUser } from "@/lib/follow-service";
+import { revalidatePath } from "next/cache";
+
+export const onFollow = async (id: string) => {
+  try {
+    const followedUser = await followUser(id);
+    revalidatePath("/");
+    if (followedUser) {
+      revalidatePath(`/${followedUser.following.username}`);
+    }
+
+    return followedUser;
+  } catch (err) {
+    throw new Error("Internal Error");
+  }
+};
+
+export const onUnfollow = async (id: string) => {
+  try {
+    const unfollowedUser = await unfollowUser(id);
+    revalidatePath("/");
+    if (unfollowedUser) {
+      revalidatePath(`/${unfollowedUser.following.username}`);
+    }
+
+    return unfollowedUser;
+  } catch (err) {
+    throw new Error("Internal Error");
+  }
+};
